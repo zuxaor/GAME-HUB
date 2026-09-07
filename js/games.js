afccfd0d06c8621f620a9234999979b033bf1805
zuxaor <adrienfemenia8@gmail.com>
2026-09-07 15:27:54 +0200

    Update games.js
    Ajout des données des jeux

--- js/games.js
+++ js/games.js
@@ -1,1 +1,122 @@
-
+[
+  {
+    "id": "fortnite",
+    "name": "Fortnite",
+    "genre": "Battle Royale",
+    "platforms": ["PC", "PS5", "Xbox Series", "Switch"],
+    "developer": "Epic Games",
+    "status": "En ligne",
+    "description": "Battle royale gratuit mêlant construction, combat et événements réguliers.",
+    "image": "assets/images/games/fortnite.svg"
+  },
+  {
+    "id": "call-of-duty",
+    "name": "Call of Duty",
+    "genre": "FPS",
+    "platforms": ["PC", "PS5", "Xbox Series"],
+    "developer": "Activision",
+    "status": "En ligne",
+    "description": "FPS militaire proposant des campagnes, du multijoueur et des modes compétitifs.",
+    "image": "assets/images/games/call-of-duty.svg"
+  },
+  {
+    "id": "rainbow-six-siege",
+    "name": "Rainbow Six Siege",
+    "genre": "FPS tactique",
+    "platforms": ["PC", "PS5", "Xbox Series"],
+    "developer": "Ubisoft",
+    "status": "En ligne",
+    "description": "FPS tactique basé sur la coopération, la stratégie et la destruction de l'environnement.",
+    "image": "assets/images/games/rainbow-six-siege.svg"
+  },
+  {
+    "id": "valorant",
+    "name": "VALORANT",
+    "genre": "FPS tactique",
+    "platforms": ["PC", "PS5", "Xbox Series"],
+    "developer": "Riot Games",
+    "status": "En ligne",
+    "description": "FPS compétitif combinant précision des armes et capacités uniques d'agents.",
+    "image": "assets/images/games/valorant.svg"
+  },
+  {
+    "id": "minecraft",
+    "name": "Minecraft",
+    "genre": "Sandbox",
+    "platforms": ["PC", "PS5", "Xbox Series", "Switch"],
+    "developer": "Mojang",
+    "status": "En ligne",
+    "description": "Jeu sandbox permettant de construire, explorer et survivre dans des mondes générés.",
+    "image": "assets/images/games/minecraft.svg"
+  },
+  {
+    "id": "gta-v",
+    "name": "GTA V",
+    "genre": "Action",
+    "platforms": ["PC", "PS5", "Xbox Series"],
+    "developer": "Rockstar Games",
+    "status": "En ligne",
+    "description": "Jeu d'action en monde ouvert avec campagne solo et expérience multijoueur GTA Online.",
+    "image": "assets/images/games/gta-v.svg"
+  },
+  {
+    "id": "rocket-league",
+    "name": "Rocket League",
+    "genre": "Sport",
+    "platforms": ["PC", "PS5", "Xbox Series", "Switch"],
+    "developer": "Psyonix",
+    "status": "En ligne",
+    "description": "Football spectaculaire dans lequel des voitures propulsées s'affrontent sur le terrain.",
+    "image": "assets/images/games/rocket-league.svg"
+  },
+  {
+    "id": "apex-legends",
+    "name": "Apex Legends",
+    "genre": "Battle Royale",
+    "platforms": ["PC", "PS5", "Xbox Series", "Switch"],
+    "developer": "Respawn Entertainment",
+    "status": "En ligne",
+    "description": "Battle royale en équipe basé sur des personnages aux capacités différentes.",
+    "image": "assets/images/games/apex-legends.svg"
+  },
+  {
+    "id": "league-of-legends",
+    "name": "League of Legends",
+    "genre": "MOBA",
+    "platforms": ["PC"],
+    "developer": "Riot Games",
+    "status": "En ligne",
+    "description": "MOBA compétitif opposant deux équipes de champions dans des parties stratégiques.",
+    "image": "assets/images/games/league-of-legends.svg"
+  },
+  {
+    "id": "the-isle",
+    "name": "The Isle",
+    "genre": "Survie",
+    "platforms": ["PC"],
+    "developer": "Afterthought LLC",
+    "status": "Accès anticipé",
+    "description": "Jeu de survie multijoueur où les joueurs incarnent des dinosaures dans un environnement hostile.",
+    "image": "assets/images/games/the-isle.svg"
+  },
+  {
+    "id": "elden-ring",
+    "name": "Elden Ring",
+    "genre": "Action-RPG",
+    "platforms": ["PC", "PS5", "Xbox Series"],
+    "developer": "FromSoftware",
+    "status": "Disponible",
+    "description": "Action-RPG en monde ouvert mêlant exploration, combats exigeants et univers fantastique.",
+    "image": "assets/images/games/elden-ring.svg"
+  },
+  {
+    "id": "counter-strike-2",
+    "name": "Counter-Strike 2",
+    "genre": "FPS tactique",
+    "platforms": ["PC"],
+    "developer": "Valve",
+    "status": "En ligne",
+    "description": "FPS compétitif emblématique basé sur le tir tactique et le travail d'équipe.",
+    "image": "assets/images/games/counter-strike-2.svg"
+  }
+]
